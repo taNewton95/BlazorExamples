7bf4dc753f26e96703cf0ca630f047113a9adc5a
Tom Newton <tanewton95@gmail.com>
2021-01-25 20:51:59 +0000

    Blazing Pizza
    https://github.com/dotnet-presentations/blazor-workshop/blob/master/docs/02-customize-a-pizza.md#component-events

---
+++ BlazingPizza/BlazingPizza.Client/Pages/Index.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Pages
+{
+    public partial class Index
+    {
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        List<PizzaSpecial> specials;
+        Pizza configuringPizza;
+        bool showingConfigureDialog;
+
+        protected override async Task OnInitializedAsync()
+        {
+            specials = await HttpClient.GetFromJsonAsync<List<PizzaSpecial>>("specials");
+        }
+
+        void ShowConfigurePizzaDialog(PizzaSpecial special)
+        {
+            configuringPizza = new Pizza()
+            {
+                Special = special,
+                SpecialId = special.Id,
+                Size = Pizza.DefaultSize,
+                Toppings = new List<PizzaTopping>(),
+            };
+
+            showingConfigureDialog = true;
+        }
+
+        public void CancelConfigurePizzaDialog()
+        {
+            configuringPizza = null;
+            showingConfigureDialog = false;
+        }
+
+    }
+}
---
+++ BlazingPizza/BlazingPizza.Client/Shared/ConfigurePizzaDialog.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Client.Shared
+{
+    public partial class ConfigurePizzaDialog
+    {
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        [Parameter]
+        public Pizza Pizza { get; set; }
+
+        List<Topping> toppings;
+
+        [Parameter] public EventCallback OnCancel { get; set; }
+        [Parameter] public EventCallback OnConfirm { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            toppings = await HttpClient.GetFromJsonAsync<List<Topping>>("toppings");
+        }
+
+        void ToppingSelected(ChangeEventArgs e)
+        {
+            if (int.TryParse((string)e.Value, out var index) && index >= 0)
+            {
+                AddTopping(toppings[index]);
+            }
+        }
+
+        void AddTopping(Topping topping)
+        {
+            if (Pizza.Toppings.Find(pt => pt.Topping == topping) == null)
+            {
+                Pizza.Toppings.Add(new PizzaTopping() { Topping = topping });
+            }
+        }
+
+        void RemoveTopping(Topping topping)
+        {
+            Pizza.Toppings.RemoveAll(pt => pt.Topping == topping);
+        }
+
+    }
+}
