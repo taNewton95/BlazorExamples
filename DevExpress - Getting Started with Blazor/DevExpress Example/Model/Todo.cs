83a01b037625e8bbcf53050b22450c955aa8e17a
Tom Newton <thomas.newton@orderwise.co.uk>
2021-01-27 12:35:51 +0000

    Getting Started with Blazor

---
+++ DevExpress - Getting Started with Blazor/DevExpress Example/Model/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevExpress_Example.Model
+{
+    public class Todo
+    {
+
+        public string Text { get; set; }
+
+        public bool Done { get; set; }
+
+    }
+}
